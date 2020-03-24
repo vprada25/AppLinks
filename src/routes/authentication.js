102c8d60e7df002a06f105346c0987c0547babff
victor <v.prada@udla.edu.co>
2020-03-24 18:32:13 -0500

    feat: add routes logged

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,38 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const { isLoggedIn } = require('../lib/auth');
+const { isNotLoggedIn } = require('../lib/auth');
 
 
-
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
     res.render('auth/signup');
 });
 
 
 
-router.post('/signup', passport.authenticate('local.signup',
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup',
     {
         successRedirect: '/profile',
         failureRedirect: '/signup',
         failureFlash: true
     }
-    ));
+));
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLoggedIn, (req, res) => {
     res.render('auth/signin');
 });
 
 
-router.post('/signin', passport.authenticate('local.signin',
+router.post('/signin', isNotLoggedIn, passport.authenticate('local.signin',
     {
         successRedirect: '/profile',
         failureRedirect: '/signin',
         failureFlash: true
     }
-    ));
+));
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile');
+});
+
+router.get('/logout',isLoggedIn, (req, res) => {
+    req.logOut();
+    res.render('auth/signin');
 });
 
 
